62d80a8307d3a207de7d05d58557b8b73bad03c9
Filippo Federico <filippofederico@mbp-filippo.intern>
2019-05-27 18:20:50 +0200

    some JS

--- weather-app.js
+++ weather-app.js
@@ -1,6 +1,6 @@
 // api key = 83b3dbffead2223ddf32aa025f2e5f66
 
-var city = 'Berlin'
+var city = 'milan'
 
 function fetchApi() {
 
@@ -16,7 +16,7 @@ function fetchApi() {
             
 
         buildBoxInfo(data)
-        
+        console.log(data.weather[0].icon);
         });
 
 }
@@ -26,10 +26,12 @@ fetchApi()
 
 function buildBoxInfo(data) {
     
-    // set the name of the city
+    // set the name of the city + country
     var nameCityHTML = document.querySelector('#city_name');
     var nameCityAPI = data.name;
-    nameCityHTML.innerHTML = nameCityAPI;
+    var nameCountryAPI = data.sys.country;
+    console.log(nameCountryAPI);
+    nameCityHTML.innerHTML = nameCityAPI + ' (' + nameCountryAPI + ')';
     
     // set the temperature
     var temperature = document.querySelector('#temp');
@@ -38,8 +40,25 @@ function buildBoxInfo(data) {
     var tempShort = temp - 273.15;
     var tempShortFixed = tempShort.toFixed(1);
     console.log(tempShortFixed);
+    temperature.innerHTML = tempShortFixed + ' °C';
     
+    // set the MAX temperature
+    var temperatureMax = document.querySelector('#temp_max');
+    var tempMax = data.main.temp_max;
+    console.log(tempMax);
+    var tempMaxShort = tempMax - 273.15;
+    var tempMaxShortFixed = tempMaxShort.toFixed(1);
+    console.log(tempMaxShortFixed);
+    temperatureMax.innerHTML = 'Temp MAX: ' + tempMaxShortFixed + ' °C';
     
+    // set the MIN temperature
+    var temperatureMin = document.querySelector('#temp_min');
+    var tempMin = data.main.temp_min;
+    console.log(tempMin);
+    var tempMinShort = tempMin - 273.15;
+    var tempMinShortFixed = tempMinShort.toFixed(1);
+    console.log(tempMinShortFixed);
+    temperatureMin.innerHTML = 'Temp MIN: ' + tempMinShortFixed + ' °C';
     
     // set the date
     var dateBox = document.querySelector('#date');
@@ -47,5 +66,13 @@ function buildBoxInfo(data) {
     var actualDate = new Date();
     console.log(actualDate);
     dateBox.innerHTML = actualDate;
+    
+    // set the img icon
+//    var imgIcon = document.querySelector('#img_icon');
+//    var iconUrl = 'http://openweathermap.org/img/w/10d.png';
+//    imgIcon.setAttribute('src', iconUrl);
+    
+    
+    
 }
 

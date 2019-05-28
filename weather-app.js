dde9706ba5ae1f50ad382d0fc1cc45d6abd51912
Filippo Federico <filippofederico@mbp-filippo.intern>
2019-05-28 12:24:38 +0200

    onclick function

--- weather-app.js
+++ weather-app.js
@@ -1,6 +1,5 @@
-// api key = 83b3dbffead2223ddf32aa025f2e5f66
 
-var city = 'miami'
+var city = 'berlin';
 
 function fetchApi() {
 
@@ -13,10 +12,9 @@ function fetchApi() {
         })
         .then(function (data) {
             console.log(data);
-            
+        
+            buildBoxInfo(data);
 
-        buildBoxInfo(data)
-        console.log(data.weather[0].icon);
         });
 
 }
@@ -25,14 +23,15 @@ fetchApi()
 
 
 function buildBoxInfo(data) {
-    
+
     // set the name of the city + country
+    
     var nameCityHTML = document.querySelector('#city_name');
     var nameCityAPI = data.name;
     var nameCountryAPI = data.sys.country;
     console.log(nameCountryAPI);
     nameCityHTML.innerHTML = nameCityAPI + ' (' + nameCountryAPI + ')';
-    
+
     // set the temperature
     var temperature = document.querySelector('#temp');
     var temp = data.main.temp;
@@ -41,7 +40,7 @@ function buildBoxInfo(data) {
     var tempShortFixed = tempShort.toFixed(1);
     console.log(tempShortFixed);
     temperature.innerHTML = tempShortFixed + ' °C';
-    
+
     // set the MAX temperature
     var temperatureMax = document.querySelector('#temp_max');
     var tempMax = data.main.temp_max;
@@ -50,7 +49,7 @@ function buildBoxInfo(data) {
     var tempMaxShortFixed = tempMaxShort.toFixed(1);
     console.log(tempMaxShortFixed);
     temperatureMax.innerHTML = 'Temp MAX: ' + tempMaxShortFixed + ' °C';
-    
+
     // set the MIN temperature
     var temperatureMin = document.querySelector('#temp_min');
     var tempMin = data.main.temp_min;
@@ -59,59 +58,79 @@ function buildBoxInfo(data) {
     var tempMinShortFixed = tempMinShort.toFixed(1);
     console.log(tempMinShortFixed);
     temperatureMin.innerHTML = 'Temp MIN: ' + tempMinShortFixed + ' °C';
-    
+
     // set the current date
     var dateBox = document.querySelector('#date');
     console.log(dateBox);
     var actualDate = new Date();
     console.log(actualDate);
     dateBox.innerHTML = actualDate;
-    
+
     // set the img icon
     var imgIcon = document.querySelector('#img_icon');
-    
-    
-    
+
+
+
     var weatherCondition = data.weather[0].description;
     console.log(weatherCondition);
-    
+
     var weatherMainCondition = data.weather[0].main;
     console.log(weatherMainCondition);
-    
-//    var iconUrl = 'http://openweathermap.org/img/w/' + icon +'';
-    
-    if(weatherCondition == 'light rain'){
-//        var icon = '50d.png';
+
+    //    var iconUrl = 'http://openweathermap.org/img/w/' + icon +'';
+
+    if (weatherCondition == 'light rain') {
         var iconUrl = 'http://openweathermap.org/img/w/10d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'clear sky'){
-//        var icon = '11d.png';
+        
+    } else if (weatherCondition == 'clear sky') {
         var iconUrl = 'http://openweathermap.org/img/w/01d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'few clouds'){
+        
+    } else if (weatherCondition == 'few clouds') {
         var iconUrl = 'http://openweathermap.org/img/w/02d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'scattered clouds'){
+        
+    } else if (weatherCondition == 'scattered clouds') {
         var iconUrl = 'http://openweathermap.org/img/w/03d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'broken clouds'){
+        
+    } else if (weatherCondition == 'broken clouds') {
         var iconUrl = 'http://openweathermap.org/img/w/04d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'shower rain'){
+        
+    } else if (weatherCondition == 'shower rain') {
         var iconUrl = 'http://openweathermap.org/img/w/09d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'thunderstorm'){
+        
+    } else if (weatherCondition == 'thunderstorm') {
         var iconUrl = 'http://openweathermap.org/img/w/11d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'snow'){
+        
+    } else if (weatherCondition == 'snow') {
         var iconUrl = 'http://openweathermap.org/img/w/13d.png';
         imgIcon.setAttribute('src', iconUrl);
-    } else if(weatherCondition == 'mist'){
+        
+    } else if (weatherCondition == 'mist') {
         var iconUrl = 'http://openweathermap.org/img/w/50d.png';
         imgIcon.setAttribute('src', iconUrl);
     }
-    
-    
-    
+
 }
-//document.body.style.backgroundImage = "url('img_tree.png')";
+
+
+function filterCity(data) {
+    
+    console.log('event')
+    console.log(event);
+
+    var inputCityHTML = document.querySelector('#input_city');
+    var searchBtn = document.querySelector('#search-button');
+
+    var valueFromInputHTML = inputCityHTML.value;
+    console.log(valueFromInputHTML);
+
+    city = valueFromInputHTML;
+
+    fetchApi()
+}
